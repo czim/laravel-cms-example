a0dd3d17885d81c6388449641f4631d608111b57
Coen Zimmerman <coen@pixelindustries.com>
2017-01-22 14:59:10 +0100

    CMS theme package updated, custom header title added

---
+++ config/cms-theme.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | CMS Header
+    |--------------------------------------------------------------------------
+    |
+    | Customize how the CMS header / logo area appears.
+    |
+    */
+
+    'header' => [
+
+        // The title to show in the header, if no custom partial has been defined.
+        'title' => 'Example CMS',
+
+        // The view partial to use in place of the CMS header/logo area.
+        // If this is set, the title above will only be used if the partial uses it.
+        'partial' => false,
+
+    ],
+
+];
